d48e6628529f1c935349b3357e1cbd9d43bea608
Arthur.chx <achaix@edenschool.fr>
2022-12-05 11:10:19 +0100

    gg

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+let num = prompt('choisi ton nombre');
+
+// alert(num); affichage du nombre choisi
+
+let nb = Math.floor(100*Math.random());
+
+
+while () {
+    if (num > nb) {
+        alert('plus petit')
+    } else if (num < nb) {
+        alert('plus grand ')
+    } else if (num === nb) {
+        alert ('bravo tu a gagner')
+    }
+}
+console.log(nb);
